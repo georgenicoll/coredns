0b9d9adf7f81fee7cd32bd2fdff2c7d49e75b1a9
George Nicoll <george.nicoll@gmail.com>
2021-10-04 23:02:40 +0100

    Switch to using name.namespace in dns

--- plugin/k8s_ext_dhcp/handler.go
+++ plugin/k8s_ext_dhcp/handler.go
@@ -58,57 +58,53 @@ func (h Handler) ServeDNS(ctx context.Context, w dns.ResponseWriter, r *dns.Msg)
 	}
 
 	//find the service part (this is the part prior to the zone, minus a separating '.' [hence the -1])
-	serviceName := qname[:len(qname)-len(zone)-1]
+	serviceKey := qname[:len(qname)-len(zone)-1]
 
-	log.Debugf("k8s ext: qname=%s, zone=%s, service=%s", qname, zone, serviceName)
+	log.Debugf("k8s ext: qname=%s, zone=%s, serviceKey=%s", qname, zone, serviceKey)
 
-	//Find the first service with a label that matches...
-	//NOTE:  should make this more efficient in a real implementation
+	//Find any service with keys that match
 	//NOTE:  this noddy implementation only handles ipv4
-	for _, svc := range h.kube.APIConn.ServiceList() {
-		if serviceName == svc.ExternalName && len(svc.ExternalIPs) > 0 {
-			records := h.serviceRecords(svc, &w, &state)
-			extra := make([]dns.RR, 0)
+	services := h.kube.APIConn.SvcIndex(serviceKey)
 
-			if len(records) == 0 {
-				return plugin.BackendError(ctx, h.kube, zone, dns.RcodeSuccess, state, nil, plugin.Options{})
-			}
+	records := h.serviceRecords(services, &w, &state)
 
-			m := new(dns.Msg)
-			m.SetReply(r)
-			m.Authoritative = true
-			m.Answer = append(m.Answer, records...)
-			m.Extra = append(m.Extra, extra...)
-			w.WriteMsg(m)
-			return dns.RcodeSuccess, nil
+	if len(records) == 0 {
+		if h.continueOnNoMatch {
+			return plugin.NextOrFailure(h.Name(), h.next, ctx, w, r)
+		} else {
+			return plugin.BackendError(ctx, h.kube, zone, dns.RcodeSuccess, state, nil, plugin.Options{})
 		}
 	}
 
-	//Nothing found call the next or fail depending on settings
-	if h.continueOnNoMatch {
-		return plugin.NextOrFailure(h.Name(), h.next, ctx, w, r)
-	}
-	return plugin.BackendError(ctx, h.kube, zone, dns.RcodeSuccess, state, nil, plugin.Options{})
+	extra := make([]dns.RR, 0)
+
+	m := new(dns.Msg)
+	m.SetReply(r)
+	m.Authoritative = true
+	m.Answer = append(m.Answer, records...)
+	m.Extra = append(m.Extra, extra...)
+	w.WriteMsg(m)
+	return dns.RcodeSuccess, nil
 }
 
-// Name implements the Handler interface.
+// ServeDNS implements the plugin.Handler interface.
 func (h Handler) Name() string { return pluginName }
 
 //Populates a record for each external ip in the service
-func (h Handler) serviceRecords(service *object.Service, w *dns.ResponseWriter, request *request.Request) (records []dns.RR) {
-
-	for _, ipAddress := range service.ExternalIPs {
-		switch request.QType() {
-		case dns.TypeA:
-			ip := net.ParseIP(ipAddress)
-			a := &dns.A{Hdr: dns.RR_Header{Name: request.QName(), Rrtype: dns.TypeA, Class: dns.ClassINET, Ttl: h.ttlSeconds}, A: ip}
-			records = append(records, a)
-		case dns.TypeAAAA:
-			// FIXME:  add to handle ipv6
-		case dns.TypeCNAME:
-			continue
+func (h Handler) serviceRecords(services []*object.Service, w *dns.ResponseWriter, request *request.Request) (records []dns.RR) {
+	for _, service := range services {
+		for _, ipAddress := range service.ExternalIPs {
+			switch request.QType() {
+			case dns.TypeA:
+				ip := net.ParseIP(ipAddress)
+				a := &dns.A{Hdr: dns.RR_Header{Name: request.QName(), Rrtype: dns.TypeA, Class: dns.ClassINET, Ttl: h.ttlSeconds}, A: ip}
+				records = append(records, a)
+			case dns.TypeAAAA:
+				// FIXME:  add to handle ipv6
+			case dns.TypeCNAME:
+				continue
+			}
 		}
-
 	}
 
 	return records
